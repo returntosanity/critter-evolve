428979b20ec58e0704688cfc9705da25c4a3ba40
returntosanity <92617865+returntosanity@users.noreply.github.com>
2022-05-12 15:25:21 +0200

    Update world.js

--- js/world.js
+++ js/world.js
@@ -1,14 +1,14 @@
 /*
 The map/world in which the critters live
 */
-let height=1000;
-let width=1000;
+let height=10;
+let width=10;
 var c1= new Critter();
 
 
 function walk()
 {
-  c1.walk_forward();
+  c1.walk_forward(width-1, height-1);
   console.log(c1);
 }
 
@@ -17,19 +17,29 @@ function turnR()
   c1.turn_right();
   console.log(c1);
 }
+function turnL()
+{
+  c1.turn_left();
+  console.log(c1);
+}
 
 function randomAction()
 {
   function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
-}
+  }
 
 var rndInt = randomIntFromInterval(1, 6)
 console.log(rndInt)
 if(rndInt<=4)
-{walk();
-console.log("decided to walk a bit")}
-else
+{
+	console.log("decided to walk a bit");
+	walk();
+}
+if(rndInt==5)
 {turnR();
 console.log("decided to turn right");}
+if(rndInt==6)
+{turnL();
+console.log("decided to turn left");}
 }
